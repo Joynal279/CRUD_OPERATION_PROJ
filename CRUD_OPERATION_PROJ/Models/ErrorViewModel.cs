eaf6df33968a2f3eb6885db60837ffa1f762124c
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-17 17:46:09 +0600

    Initial commit

---
+++ CRUD_OPERATION_PROJ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUD_OPERATION_PROJ.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
