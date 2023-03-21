8909fc82bac0149c3afa6ec79c5603d604a3a490
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-21 07:41:35 +0600

    changed connection strings

--- CRUD_OPERATION_PROJ/Migrations/ApplicationDbContextModelSnapshot.cs
+++ CRUD_OPERATION_PROJ/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,10 +17,10 @@ namespace CRUD_OPERATION_PROJ.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.9")
+                .HasAnnotation("ProductVersion", "7.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
-            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
+            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
             modelBuilder.Entity("CRUD_OPERATION_PROJ.Models.Employee", b =>
                 {
@@ -28,7 +28,7 @@ namespace CRUD_OPERATION_PROJ.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int");
 
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
                     b.Property<string>("Address")
                         .IsRequired()
